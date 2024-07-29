e891d008a082dcb99eed92702bba89297f273643
Gustavo Teodoro <gustavoteodsant@gmail.com>
2024-07-29 20:14:02 -0300

    correção do texto da introdução

--- src/components/Inicio/Introducao.jsx
+++ src/components/Inicio/Introducao.jsx
@@ -2,7 +2,8 @@ function Introducao (){
     return (
         <div>
             <h2>Vantagens e Benefícios</h2>
-            <p>A Hortomática foi projetada para ser simples e eficiente. Possuindo um formato de uma “caixa”, onde a terra é posta, onde por baixo há uma base impermeável, onde ficam os componentes que tornam a Hortomática automatizada. Com sensores que monitoram constantemente o nível de umidade, garantindo que a planta receba a quantidade ideal de água. O usuário registra o tipo de hortaliça que está cultivando por meio de um aplicativo intuitivo, disponível para dispositivos móveis. </p>
+            <p>A Hortomática oferece uma série de vantagens e benefícios que tornam o cultivo de hortaliças em ambientes urbanos não apenas possível, mas também extremamente conveniente. Primeiramente, ela permite o cultivo de temperos frescos e orgânicos em casa, promovendo uma alimentação mais saudável e sustentável. A automação do processo de irrigação elimina a preocupação com a rega inadequada, economizando tempo e garantindo que as plantas recebam exatamente a quantidade de água necessária.</p>
+            <p>Além disso, o uso eficiente de água contribui para a sustentabilidade ambiental. Outro benefício significativo é a facilidade de uso do aplicativo, que proporciona uma experiência de jardinagem moderna e sem complicações. A Hortomática é também uma excelente opção para quem tem pouco espaço, pois seu design compacto é perfeito para pequenos apartamentos e casas urbanas.</p>
         </div>
     )
 }
