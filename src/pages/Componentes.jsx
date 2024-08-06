d66da91195cdac866795b290252fb27eab747f25
Gustavo Teodoro <gustavoteodsant@gmail.com>
2024-08-05 22:19:39 -0300

    adição da pagina de componentes

--- src/pages/Componentes.jsx
+++ src/pages/Componentes.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import BarraFixa from '../components/BarraFixa'
+import placaArduino from '/placa-arduino.png'
+import jumpers from '/jumpers.png'
+import fonteAlimentacao from '/fonte-alimentacao.png'
+import moduloRele from '/modulo-rele.png'
+import fonteChaveada from '/fonte-chaveada.png'
+import adaptadorFemea from '/adaptador-femea.png'
+import sensorUmidade from '/sensor-de-umidade.png'
+import miniBomba from '/mini-bomba-dagua.png'
+import mangueiraAquario from '/mangueira-aquario.png'
+import fioParalelo from '/fio-paralelo.png'
 
 function Componentes() {
   return (
     <div>
+        <BarraFixa />
       <h1>Componentes</h1>
+      <section>
+            <li>Placa Arduino Uno: Serve para ligar a parte física do digital</li>
+            <img src="/placa-arduino.png" alt="" />
+      </section>
+      <section>
+            <li>Jumpers: servem para fazer as conexões entre diversos componentes</li>
+            <img src={jumpers} alt="" />
+      </section>
+      <section>
+            <li>Fonte de Alimentação Arduino 9VDC 1A: É utilizado para alimentar o Arduino Uno</li>
+            <img src={fonteAlimentacao} alt="" />
+      </section>
+      <section>
+            <li>Módulo Relé 5V 10A: Acionar e desacionar cargas</li>
+            <img src={moduloRele} alt="" />
+      </section>
+      <section>
+            <li>Fonte Chaveada 12V 1A: Fornecer energia necessária para equipamentos que requerem uma alimentação 12V com uma corrente máxima 1A</li>
+            <img src={fonteChaveada} alt="" />
+      </section>
+      <section>
+            <li>Adaptador Fêmea com conexões para plug P4: Serve para fazer uma ligação com a Fonte Chaveada. P4 é uma fonte que converte tensões</li>
+      </section>
+      <section>
+            <li>Sensor de Umidade: Serve para medir a umidade atual do solo. Funcionando pelo contado com o solo e fazendo uma leitura da umidade relativa do solo por comparação com a resistência</li>
+            <img src={sensorUmidade} alt="" />
+      </section>
+      <section>
+            <li>Mini Bomba de água: Possui pequenos pistões que se movem de forma cordenada, assim fazendo com que a água seja bombeada</li>
+            <img src={miniBomba} alt="" />
+      </section>
+      <section>
+            <li>Mangueira de Aquário: Serve para transferir a água para onde for nessessário</li>
+            <img src={mangueiraAquario} alt="" />
+      </section>
     </div>
   )
 }
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,16 +1,18 @@
-import ConhecaHortomatica from "../components/Inicio/ConhecaHortomatica"
-import Introducao from "../components/Inicio/Introducao"
 import BarraFixa from "../components/BarraFixa"
+import imgInicio from "/inicio-img.png"
 
 function Inicio() {
   return (
     <div>
-      <h1>Inicio</h1>
       <BarraFixa />
-      <ConhecaHortomatica />
-      <Introducao />
+      <h1>Conheça a Hortomatica</h1>
+      <p>Bem-vindo ao futuro sustentável da jardinagem urbana com a Hortomática, um inovador protótipo em Arduino que traz uma solução inteligente e automatizada para o cultivo de hortaliças em ambientes compactos. </p>
+      <img src={imgInicio} alt="Imagem ilustrativa do projeto" className="w-3/12"/>
+      <h2>Vantagens e Benefícios</h2>
+      <p>A Hortomática oferece uma série de vantagens e benefícios que tornam o cultivo de hortaliças em ambientes urbanos não apenas possível, mas também extremamente conveniente. Primeiramente, ela permite o cultivo de temperos frescos e orgânicos em casa, promovendo uma alimentação mais saudável e sustentável. A automação do processo de irrigação elimina a preocupação com a rega inadequada, economizando tempo e garantindo que as plantas recebam exatamente a quantidade de água necessária.</p>
+      <p>Além disso, o uso eficiente de água contribui para a sustentabilidade ambiental. Outro benefício significativo é a facilidade de uso do aplicativo, que proporciona uma experiência de jardinagem moderna e sem complicações. A Hortomática é também uma excelente opção para quem tem pouco espaço, pois seu design compacto é perfeito para pequenos apartamentos e casas urbanas.</p>
     </div>
   )
 }
 
-export default Inicio;
+export default Inicio
