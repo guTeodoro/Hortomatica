8a20693796fcab146a208aad1d41a34b68af4eef
EA - Thiago Teodoro Santana <thiago_teodoro@educadventista.org>
2024-08-19 09:58:35 -0300

    segunda tentativa de correção da rota

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
 import Inicio from "./pages/Inicio"
 import Componentes from "./pages/Componentes"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter } from "react-router-dom"
+import { Link } from "react-router-dom"
 import Funcionamento from "./pages/Funcionamento"
 import Referencias from "./pages/Referencias"
 import Integrantes from "./pages/Integrantes"
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Inicio />
+  },
+  {
+    path: "/componentes",
+    element: <Componentes />
+  },
+  {
+    path: "/referencias",
+    element: <Referencias />
+  },
+  {
+    path: "/integrantes",
+    element: <Integrantes />
+  },
+  {
+    path: "/funcionamento",
+    element: <Funcionamento />
+  },
+])
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Inicio />}/>
-        <Route path="/componentes" element={<Componentes />}/>
-        <Route path="/funcionamento" element={<Funcionamento />}/>
-        <Route path="/referencias" element={<Referencias />}/>
-        <Route path="/integrantes" element={<Integrantes />}/>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   )
 }
--- src/components/BarraFixa.jsx
+++ src/components/BarraFixa.jsx
@@ -1,16 +1,18 @@
+import { Link } from "react-router-dom"
 import iconHortomatica from "../assets/icon-hortomatica.png"
 import menuburger from "/menu-burger.png"
 
-function BarraFixa () {
-    return (       
+function BarraFixa() {
+    return (
         <div className="border-b-8 border-green-400 fixed top-0 left-0 right-0 z-20" >
-            <div className="bg-black flex h-[72px] border-b-8 border-purple-700 z-30 lg:h-24">
-                <a href="/" className="flex justify-center items-center ml-0 mt-1 "><img src={iconHortomatica} alt="Voltar ao início" className="w-10 mt-7 z-20 lg:w-16" /> <h1 className="text-white ml-2 mt-3 text-2xl font-semibold lg:text-5xl">Hortomática</h1></a>        
+            <div className="bg-black flex h-[72px] border-b-8 border-purple-700 z-30 lg:h-24 list-none">
+                <a href="/" className="flex justify-center items-center ml-0 mt-1 "><img src={iconHortomatica} alt="Voltar ao início" className="w-10 mt-7 z-20 lg:w-16" /> <h1 className="text-white ml-2 mt-3 text-2xl font-semibold lg:text-5xl">Hortomática</h1></a>
                 <img src={menuburger} alt="" srcset="" className="w-9 h-9 mt-6 ml-[36%] lg:hidden" />
-                <a href="/integrantes" className="text-white mob:hidden ml-[32%] font-semibold mt-10">Integrantes</a>
-                <a href="/funcionamento" className="text-white mob:hidden ml-12 font-semibold mt-10">Funcionamento</a>
-                <a href="/componentes" className="text-white mob:hidden ml-12 font-semibold mt-10">Componentes</a>
-                <a href="/referencias" className="text-white mob:hidden ml-12 font-semibold mt-10">Referências</a>
+                <li><Link to={'/'}>Inicio</Link></li>
+                <li><Link to={'/componentes'} className="text-white mob:hidden ml-12 font-semibold mt-10">Componentes</Link></li>
+                <li><Link to={'/referencias'} className="text-white mob:hidden ml-12 font-semibold mt-10">Referencias</Link></li>
+                <li><Link to={'/integrantes'} className="text-white mob:hidden ml-[32%] font-semibold mt-10">Integrantes</Link></li>
+                <li><Link to={'/funcionamento'} className="text-white mob:hidden ml-12 font-semibold mt-10">Funcionamento</Link></li>
             </div>
         </div>
     )
