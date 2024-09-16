48535fea54efc08ea3e6d76c09c9b523e13a3670
EA - Thiago Teodoro Santana <thiago_teodoro@educadventista.org>
2024-09-16 09:56:34 -0300

    site 92%

--- src/pages/Funcionamento.jsx
+++ src/pages/Funcionamento.jsx
@@ -11,32 +11,43 @@ function Funcionamento() {
       <BarraFixa />
       <section className="flex items-center justify-center bg-[url('/inicio-img.png')] bg-cover bg-top w-full h-[272px] lg:bg-none lg:flex-row">
 
-        <section className="bg-black h-24 flex items-center justify-center w-72 mt-14 relative z-10 lg:h-28 lg:mt-40 lg:w-96 lg:mr-40 lg:ml-10">
-          <h1 className="text-white w-40 text-2xl font-bold text-center mr-2 mb-2 lg:text-4xl lg:w-72">Como ela funciona ?</h1>
+            <section className="bg-black h-24 flex items-center justify-center w-72 mt-14 relative z-10 lg:h-28 lg:mt-16 lg:w-96 lg:mr-40 lg:ml-10 lg:hidden">
+              <h1 className="text-white w-40 text-2xl font-bold text-center mr-2 mb-2 lg:text-4xl lg:w-72">Como ela funciona ?</h1>
+            </section>
+
+            <section className="bg-purple-700 w-72 h-24 ml-5 mt-20 absolute z-0 lg:h-28 lg:mt-20 lg:mr-[722px] lg:w-96 lg:ml-14 lg:hidden">
+              <p className="text-purple-700">a</p>
+            </section>
+
+        <section className="block mt-96 justify-center items-center mob:hidden">
+          <section className="flex">
+            <section className="bg-black h-24 flex items-center justify-center w-72 mt-14 relative z-10 lg:h-28 lg:mt-16 lg:w-96 lg:mr-40 lg:ml-10">
+              <h1 className="text-white w-40 text-2xl font-bold text-center mr-2 mb-2 lg:text-4xl lg:w-72">Como ela funciona ?</h1>
+            </section>
+
+            <section className="bg-purple-700 w-72 h-24 ml-5 mt-20 absolute z-0 lg:h-28 lg:mt-20 lg:mr-[722px] lg:w-96 lg:ml-14">
+              <p className="text-purple-700">a</p>
+            </section>
+          </section>
+          <p className='text-black font-bold w-[464px] leading-5 mob:hidden mt-24 ml-0 text-center'>A Hortomática foi projetada para ser simples e eficiente. Possuindo um formato de uma “caixa”, onde a terra é posta, onde por baixo há uma base impermeável, onde ficam os componentes que tornam a Hortomática automatizada. Com sensores que monitoram constantemente o nível de umidade, garantindo que a planta receba a quantidade ideal de água. </p>
         </section>
 
-        <section className="bg-purple-700 w-72 h-24 ml-5 mt-20 absolute z-0 lg:h-28 lg:mt-48 lg:mr-[722px] lg:w-96 lg:ml-20">
-          <p className="text-purple-700">a</p>
-        </section>
+        <hr className='border-2 h-[422px] border-black mr-32 mt-[566px] mob:hidden'/>
 
-        <hr className='border-2 h-96 border-black mr-32 mt-[522px] mob:hidden'/>
+        <img src={imgInicio} alt="" className="mob:hidden w-[422px] mr-0 mt-[600px] z-10 border-2 border-black"/>
 
-        <img src={imgInicio} alt="" className="mob:hidden w-[422px] mr-0 mt-[500px] z-10 border-2 border-black"/>
-
-        <section className="mob:hidden bg-purple-700 w-[422px] h-[352px] ml-[775px] mt-[564px] absolute z-0">
+        <section className="mob:hidden bg-purple-700 w-[422px] h-[352px] ml-[775px] mt-[664px] absolute z-0">
           <p className="text-purple-700">a</p>
         </section>
 
       </section>
-
-      <p className='text-black font-bold w-[464px] leading-5 mob:hidden mt-24 ml-28 text-center'>A Hortomática foi projetada para ser simples e eficiente. Possuindo um formato de uma “caixa”, onde a terra é posta, onde por baixo há uma base impermeável, onde ficam os componentes que tornam a Hortomática automatizada. Com sensores que monitoram constantemente o nível de umidade, garantindo que a planta receba a quantidade ideal de água. </p>
 
       <section className='flex flex-col justify-center items-center bg-black pt-7 pb-12 text-center lg:hidden'>
         <hr className="mb-7 w-60 border-4 border-white" />
         <p className='text-white font-bold w-80 leading-5'>A Hortomática foi projetada para ser simples e eficiente. Possuindo um formato de uma “caixa”, onde a terra é posta, onde por baixo há uma base impermeável, onde ficam os componentes que tornam a Hortomática automatizada. Com sensores que monitoram constantemente o nível de umidade, garantindo que a planta receba a quantidade ideal de água. </p>
       </section>
 
-      <section className='bg-purple-700 text-purple-700 w-full h-5 mt-0 lg:mt-44 lg:bg-green-400 lg:text-green-400 lg:h-11'>
+      <section className='bg-purple-700 text-purple-700 w-full h-5 mt-0 lg:mt-[444px] lg:bg-green-400 lg:text-green-400 lg:h-11'>
             <p></p>
       </section>
 
